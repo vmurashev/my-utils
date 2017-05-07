b668d2f4999031caa889018c152c5e1a6c8701a8
vmurashev <vitaly.murashev@gmail.com>
2017-05-07 12:11:55 +0300

    asmwork - buildfix

--- asmwork/hello/hello_libc.asm
+++ asmwork/hello/hello_libc.asm
@@ -1,22 +1,71 @@
+DEFAULT REL
+
+%ifidn __OUTPUT_FORMAT__, win32
+    %define EXTERNC_UNDERSCORE
+    %define SYS_WINDOWS
+%endif
+
+%ifidn __OUTPUT_FORMAT__, win64
+    %define SYS_WINDOWS
+%endif
+
+%macro cglobal_underscore 1
+    global  _%1
+    %define %1 _%1
+%endmacro
+
+%macro cextern_underscore 1
+    extern  _%1
+    %define %1 _%1
+%endmacro
+
+%ifdef EXTERNC_UNDERSCORE
+    %define cextern cextern_underscore
+    %define cglobal cglobal_underscore
+%else
+    %define cextern extern
+    %define cglobal global
+%endif
+
+
+cextern puts
+cglobal asm_main
+
+
 section .data
 HelloMsg: db 'Hello world!',0
 
 section .text
-
-extern puts
-global asm_main
-
 asm_main:
-    push ebp
-    mov ebp,esp
-    push ebx
-    push esi
-    push edi
+%ifidn __BITS__,64            ; x86_64
+    push rbp
+    mov rbp, rsp
 
+%ifdef SYS_WINDOWS
+    lea rcx, [HelloMsg]
+    call puts
+%else
+    lea rdi, [HelloMsg]
+    call puts wrt ..plt
+%endif
+    mov rax, 42
+    leave
+    ret
+%else                         ; x86
+    push ebp
+    mov ebp, esp
+
+%ifdef SYS_WINDOWS
     push HelloMsg
     call puts
-    add esp,4
+%else
+    lea eax, [ebx + HelloMsg wrt ..gotoff]
+    push eax
+    call puts wrt ..plt
+%endif
+    add esp, 4
 
-    mov esp,ebp
-    pop ebp
+    mov eax, 42
+    leave
     ret
+%endif
--- asmwork/hello/main.c
+++ asmwork/hello/main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 extern int asm_main();
 
 int main()
 {
-    int ret = asm_main();
+    int ret = 0;
+    ret = asm_main();
+    printf("asm_main() - return code: %d\n", ret);
     return ret;
 }
